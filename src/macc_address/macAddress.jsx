253f01888415e21de4e56ba23264d0bf85f9a0dd
juratali <juratbekxudayberganov9@gmail.com>
2023-09-14 09:09:31 +0500

    changes

--- src/macc_address/macAddress.jsx
+++ src/macc_address/macAddress.jsx
@@ -1,27 +1,9 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
-const api_url = "https://macaddress.onrender.com/mac_address";
-
 const App2 = () => {
-  const [maccAddress, setMaccAddress] = useState(null);
-
-  useEffect(() => {
-    axios
-      .get(api_url)
-      .then((res) => {
-        setMaccAddress(res.data.maccAddress);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
   return (
     <div className="flex justify-center items-center min-h-[80vh]">
       <div className="flex gap-[10px] text-[20px]">
         <h1>Your mac address is:</h1>{" "}
-        <h1 className="font-bold text-red-600">
-          {maccAddress ? maccAddress : "Aniqlanmadi!"}
-        </h1>
+        <h1 className="font-bold text-red-600">{"Aniqlanmadi!"}</h1>
       </div>
     </div>
   );
